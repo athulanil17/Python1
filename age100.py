8ab5c8f3bc9ce220b347ec779bc38476f5a92a59
athulanil17 <118271019+athulanil17@users.noreply.github.com>
2023-01-12 01:33:44 +0530

    Update age100.py

--- age100.py
+++ age100.py
@@ -3,6 +3,6 @@ name=input("Enter your Name:" )
 age=int(input("Enter your age:" ))
 hage=int(cyear+(100-age))
 if age>0:
-print("Hi" ,name, "you will be 100 years old in the year", hage)
+  print("Hi" ,name, "you will be 100 years old in the year", hage)
 else:
-print("Invalid age")
+  print("Invalid age")
