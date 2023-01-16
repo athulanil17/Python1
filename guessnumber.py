2b17f7bcd04e83e568ced66d3dfde95d56f00762
athulanil17 <118271019+athulanil17@users.noreply.github.com>
2023-01-16 22:10:05 +0530

    Update guessnumber.py

--- guessnumber.py
+++ guessnumber.py
@@ -1,17 +1,17 @@
 import random
 
-number=random.randint(1,10)
+number=random.randint(1,9)
 guess=0
 count=0
 
 while guess!=number and guess!=exit:
-    guess=input("You have to guess the number between 1 to 10: ")
+    guess=input("Guess a number between 1 to 9: ")
     
     
     guess=int(guess)
     count+=1
-    if guess not in range(1,10):
-        print("You have to guess a number between 1 to 10")
+    if guess not in range(1,9):
+        print("You have to guess a number between 1 to 9")
     
     if guess==number:
         print("you guessed it right")
